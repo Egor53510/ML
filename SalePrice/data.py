be657438a301e747827ecc948c2ed871297dbe29
Egor53510 <118753017+Egor53510@users.noreply.github.com>
2024-03-25 16:54:24 +0300

    Add files via upload

---
+++ SalePrice/data.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from sklearn.metrics import mean_absolute_error
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+
+
+data = pd.read_csv('train.csv')
+data_test = pd.read_csv('test.csv')
+y = data.SalePrice
+
+features = ["LotArea", "YearBuilt", "1stFlrSF", "2ndFlrSF", "FullBath", "BedroomAbvGr", "TotRmsAbvGrd"]
+X = data[features]
+X_test = data_test[features]
+
+my_model = RandomForestRegressor(n_estimators=100, criterion='absolute_error', random_state=0)
+my_model.fit(X, y)
+preds_test = my_model.predict(X_test)
+output = pd.DataFrame({'Id': data_test.Id,
+                       'SalePrice': preds_test})
+output.to_csv('submission.csv', index=False)
+
